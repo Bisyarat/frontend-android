819c3448738aefd237328af1d840a2b963190239
Habib Al Qadri <habibalqadri50@gmail.com>
2023-10-19 06:20:51 +0700

    add: perbaikan kode validate when click submit button

--- app/src/main/java/com/dicoding/picodiploma/loginwithanimation/view/login/LoginActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/loginwithanimation/view/login/LoginActivity.kt
@@ -7,6 +7,7 @@ import android.text.Editable
 import android.util.Patterns
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -18,6 +19,7 @@ import com.dicoding.picodiploma.loginwithanimation.databinding.ActivityLoginBind
 import com.dicoding.picodiploma.loginwithanimation.view.ViewModelFactory
 import com.dicoding.picodiploma.loginwithanimation.view.main.MainActivity
 import java.util.regex.Pattern
+import kotlin.math.log
 
 class LoginActivity : AppCompatActivity() {
 
@@ -26,6 +28,8 @@ class LoginActivity : AppCompatActivity() {
     }
     private lateinit var binding: ActivityLoginBinding
 
+    var validateEmailText = false
+    var validatePasswordText = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,35 +37,56 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setupView()
-        setupAction()
-        validateWhenClickSubmitButtton()
+//        setupAction()
         rulesEditText()
+        validateWhenClickSubmitButtton()
     }
 
-    private fun validateWhenClickSubmitButtton(){
+    private fun validateWhenClickSubmitButtton() {
         binding.loginButton.setOnClickListener {
-            if (binding.emailEditText.text.toString().length == 0){
+            val emailText = binding.emailEditText.text.toString()
+            val passwordText = binding.passwordEditText.text.toString()
+
+            if (emailText.length === 0) {
                 binding.emailEditTextLayout.error = getString(R.string.errorEmptyField)
+            } else {
+                binding.emailEditTextLayout.error = null
             }
-            if (binding.passwordEditText.text.toString().length == 0){
+            if (passwordText.length === 0) {
                 binding.passwordEditTextLayout.error = getString(R.string.errorEmptyField)
+            } else {
+                binding.passwordEditTextLayout.error = null
             }
+
+            if ((validateEmailText) && (validatePasswordText)) {
+                successValidate()
+            } else{
+                Toast.makeText(this, "gagal", Toast.LENGTH_SHORT ).show()
+            }
+
+
         }
     }
-    private fun rulesEditText(){
-        binding.emailEditText.doAfterTextChanged {text ->
-            if (!Patterns.EMAIL_ADDRESS.matcher(text).matches()){
+
+    private fun rulesEditText() {
+        binding.emailEditText.doAfterTextChanged { text ->
+            if (!Patterns.EMAIL_ADDRESS.matcher(text).matches()) {
                 binding.emailEditTextLayout.error = getString(R.string.errorEmail)
-            }
-            else{
+                validateEmailText = false
+            } else {
                 binding.emailEditTextLayout.error = null
+                validateEmailText = true
             }
         }
         binding.passwordEditText.doOnTextChanged { text, start, before, count ->
-            if (text!!.length < 8){
+            if (text!!.length < 8) {
                 binding.passwordEditTextLayout.error = getString(R.string.error)
-            } else{
+                validatePasswordText = false
+            } else {
                 binding.passwordEditTextLayout.error = null
+                validatePasswordText = true
+//                Toast.makeText(this, validatePasswordText.toString(), Toast.LENGTH_SHORT ).show()
+
             }
         }
     }
@@ -80,22 +105,26 @@ class LoginActivity : AppCompatActivity() {
         supportActionBar?.hide()
     }
 
-    private fun setupAction() {
-        binding.loginButton.setOnClickListener {
-            val email = binding.emailEditText.text.toString()
-            viewModel.saveSession(UserModel(email, "sample_token"))
-            AlertDialog.Builder(this).apply {
-                setTitle("Yeah!")
-                setMessage("Anda berhasil login. Sudah tidak sabar untuk belajar ya?")
-                setPositiveButton("Lanjut") { _, _ ->
-                    val intent = Intent(context, MainActivity::class.java)
-                    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
-                    startActivity(intent)
-                    finish()
-                }
-                create()
-                show()
+//    private fun setupAction() {
+//        binding.loginButton.setOnClickListener {
+//            successValidate()
+//        }
+//    }
+
+    private fun successValidate() {
+        val email = binding.emailEditText.text.toString()
+        viewModel.saveSession(UserModel(email, "sample_token"))
+        AlertDialog.Builder(this).apply {
+            setTitle("Yeah!")
+            setMessage("Anda berhasil login. Sudah tidak sabar untuk belajar ya?")
+            setPositiveButton("Lanjut") { _, _ ->
+                val intent = Intent(context, MainActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                startActivity(intent)
+                finish()
             }
+            create()
+            show()
         }
     }
 
