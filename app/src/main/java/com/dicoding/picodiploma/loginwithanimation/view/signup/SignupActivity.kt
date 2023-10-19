5fdc4fd197ae8a4b3d7cfb6d3b06e631ec4446ad
Habib Al Qadri <habibalqadri50@gmail.com>
2023-10-19 07:12:20 +0700

    add: validation di signup

--- app/src/main/java/com/dicoding/picodiploma/loginwithanimation/view/signup/SignupActivity.kt
+++ app/src/main/java/com/dicoding/picodiploma/loginwithanimation/view/signup/SignupActivity.kt
@@ -2,14 +2,23 @@ package com.dicoding.picodiploma.loginwithanimation.view.signup
 
 import android.os.Build
 import android.os.Bundle
+import android.util.Patterns
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
+import com.dicoding.picodiploma.loginwithanimation.R
 import com.dicoding.picodiploma.loginwithanimation.databinding.ActivitySignupBinding
 
 class SignupActivity : AppCompatActivity() {
     private lateinit var binding: ActivitySignupBinding
+
+    var validateNameText = false
+    var validateEmailText = false
+    var validatePasswordText = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +27,7 @@ class SignupActivity : AppCompatActivity() {
 
         setupView()
         setupAction()
+        rulesEditText()
     }
 
     private fun setupView() {
@@ -35,17 +45,74 @@ class SignupActivity : AppCompatActivity() {
 
     private fun setupAction() {
         binding.signupButton.setOnClickListener {
-            val email = binding.emailEditText.text.toString()
+            finalValidate()
+        }
+    }
 
-            AlertDialog.Builder(this).apply {
-                setTitle("Yeah!")
-                setMessage("Akun dengan $email sudah jadi nih. Yuk, login dan belajar coding.")
-                setPositiveButton("Lanjut") { _, _ ->
-                    finish()
-                }
-                create()
-                show()
+    private fun rulesEditText() {
+        binding.emailEditText.doAfterTextChanged { text ->
+            if (!Patterns.EMAIL_ADDRESS.matcher(text).matches()) {
+                binding.emailEditTextLayout.error = getString(R.string.errorEmail)
+                validateEmailText = false
+            } else {
+                binding.emailEditTextLayout.error = null
+                validateEmailText = true
             }
+        }
+        binding.passwordEditText.doOnTextChanged { text, start, before, count ->
+            if (text!!.length < 8) {
+                binding.passwordEditTextLayout.error = getString(R.string.error)
+                validatePasswordText = false
+            } else {
+                binding.passwordEditTextLayout.error = null
+                validatePasswordText = true
+            }
+        }
+    }
+
+    private fun successValidate(){
+        val email = binding.emailEditText.text.toString()
+
+        AlertDialog.Builder(this).apply {
+            setTitle("Yeah!")
+            setMessage("Akun dengan $email sudah jadi nih. Yuk, login dan belajar coding.")
+            setPositiveButton("Lanjut") { _, _ ->
+                finish()
+            }
+            create()
+            show()
+        }
+    }
+
+    private fun finalValidate(){
+        val nameText = binding.nameEditText.text.toString()
+        val emailText = binding.emailEditText.text.toString()
+        val passwordText = binding.passwordEditText.text.toString()
+
+        if (nameText.length === 0) {
+            binding.nameEditTextLayout.error = getString(R.string.errorEmptyField)
+            validateNameText = false
+        } else {
+            binding.nameEditTextLayout.error = null
+            validateNameText = true
+        }
+
+        if (emailText.length === 0) {
+            binding.emailEditTextLayout.error = getString(R.string.errorEmptyField)
+        } else {
+            binding.emailEditTextLayout.error = null
+        }
+
+        if (passwordText.length === 0) {
+            binding.passwordEditTextLayout.error = getString(R.string.errorEmptyField)
+        } else {
+            binding.passwordEditTextLayout.error = null
+        }
+
+        if ((validateEmailText) && (validatePasswordText) && (validateNameText)) {
+            successValidate()
+        } else{
+            Toast.makeText(this, "gagal", Toast.LENGTH_SHORT ).show()
         }
     }
 }
