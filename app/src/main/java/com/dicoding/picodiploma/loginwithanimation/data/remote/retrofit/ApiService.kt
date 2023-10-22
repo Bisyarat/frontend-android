870f68abb1e921444afaf35526d119af27d56ad6
Habib Al Qadri <habibalqadri50@gmail.com>
2023-10-22 15:00:20 +0700

    add: api Service add new story

--- app/src/main/java/com/dicoding/picodiploma/loginwithanimation/data/remote/retrofit/ApiService.kt
+++ app/src/main/java/com/dicoding/picodiploma/loginwithanimation/data/remote/retrofit/ApiService.kt
@@ -1,13 +1,18 @@
 package com.dicoding.picodiploma.loginwithanimation.data.remote.retrofit
 
+import com.dicoding.picodiploma.loginwithanimation.data.remote.response.AddNewStoryResponse
 import com.dicoding.picodiploma.loginwithanimation.data.remote.response.LoginResponse
 import com.dicoding.picodiploma.loginwithanimation.data.remote.response.RegisterResponse
 import com.dicoding.picodiploma.loginwithanimation.data.remote.response.StoryResponse
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.Part
 
 interface ApiService {
     @FormUrlEncoded
@@ -27,4 +32,11 @@ interface ApiService {
 
     @GET("stories")
     suspend fun getStories(): StoryResponse
+
+    @Multipart
+    @POST("stories")
+    suspend fun addNewStory(
+        @Part file: MultipartBody.Part,
+        @Part("description") description: RequestBody,
+    ): AddNewStoryResponse
 }
