56cd2c8aa75f30180f7d1c1d978822f67a5089e1
Habib Al Qadri <habibalqadri50@gmail.com>
2023-12-13 20:57:38 +0700

    add: response user, feedback_user, kategori

---
+++ app/src/main/java/com/dicoding/picodiploma/loginwithanimation/data/remote/response/KategoriResponse.kt
@@ -0,0 +1,18 @@
+package com.dicoding.picodiploma.loginwithanimation.data.remote.response
+
+import com.google.gson.annotations.SerializedName
+
+data class KategoriResponse(
+    val listKategori: List<ListKategoriItem> = emptyList(),
+)
+
+data class ListKategoriItem (
+    @field:SerializedName("id")
+    val id: Int? = null,
+
+    @field:SerializedName("nama_kategori")
+    val namaKategori: String? = null,
+
+    @field:SerializedName("penjelasan")
+    val penjelasan: String? = null,
+)
